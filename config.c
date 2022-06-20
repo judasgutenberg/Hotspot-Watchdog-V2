236bb7d899dda959f0ec9b6502d02dee082f810d
Gus Mueller <gus@asecular.com>
2022-06-20 16:50:09 -0400

    Update config.c

--- config.c
+++ config.c
@@ -10,7 +10,6 @@ const char* hostGet = "your_server.com";
 const int locationId = 1; //for storing data from different locations in the backend
 const int secondsGranularity = 300; //how often to store data in the backend in seconds
 
-
-const int granularityWhenInConnectionFailureMode = 30;
+const int granularityWhenInConnectionFailureMode = 40;
 const int numberOfHotspotRebootsOverLimitedTimeframeBeforeEspReboot = 4;
 const int hotspotLimitedTimeFrame = 340; //seconds
